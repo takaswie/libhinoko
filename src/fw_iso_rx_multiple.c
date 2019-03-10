2f59b3d048fcfa615e5988da12deeba99e039144
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-03-11 00:12:13 +0900

    fw_iso_rx_multiple: add an GObject object to handle IR context for buffer-fill mode

---
+++ src/fw_iso_rx_multiple.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "fw_iso_rx_multiple.h"
+
+/**
+ * SECTION:fw_iso_rx_multiple
+ * @Title: HinokoFwIsoRxMultiple
+ * @Short_description: An object to receive isochronous packet for several
+ *		       channels.
+ *
+ * A #HinokoFwIsoRxMultiple receives isochronous packets for several channels by
+ * IR context for buffer-fill mode in 1394 OHCI.
+ */
+G_DEFINE_TYPE(HinokoFwIsoRxMultiple, hinoko_fw_iso_rx_multiple,
+	      HINOKO_TYPE_FW_ISO_CTX)
+
+static void hinoko_fw_iso_rx_multiple_class_init(HinokoFwIsoRxMultipleClass *klass)
+{
+	return;
+}
+
+static void hinoko_fw_iso_rx_multiple_init(HinokoFwIsoRxMultiple *self)
+{
+	return;
+}
+
+/**
+ * hinoko_fw_iso_rx_multiple_new:
+ *
+ * Instantiate #HinokoFwIsoRxMultiple object and return the instance.
+ *
+ * Returns: an instance of #HinokoFwIsoRxMultiple.
+ */
+HinokoFwIsoRxMultiple *hinoko_fw_iso_rx_multiple_new(void)
+{
+	return g_object_new(HINOKO_TYPE_FW_ISO_RX_MULTIPLE, NULL);
+}
---
+++ src/fw_iso_rx_multiple.h
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HINOKO_FW_ISO_RX_MULTIPLE_H__
+#define __HINOKO_FW_ISO_RX_MULTIPLE_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <fw_iso_ctx.h>
+
+G_BEGIN_DECLS
+
+#define HINOKO_TYPE_FW_ISO_RX_MULTIPLE	(hinoko_fw_iso_rx_multiple_get_type())
+
+#define HINOKO_FW_ISO_RX_MULTIPLE(obj)					\
+	(G_TYPE_CHECK_INSTANCE_CAST((obj),				\
+				    HINOKO_TYPE_FW_ISO_RX_MULTIPLE,	\
+				    HinokoFwIsoRxMultiple))
+#define HINOKO_IS_FW_ISO_RX_MULTIPLE(obj)				\
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj),				\
+				    HINOKO_TYPE_FW_ISO_RX_MULTIPLE))
+
+#define HINOKO_FW_ISO_RX_MULTIPLE_CLASS(klass)				\
+	(G_TYPE_CHECK_CLASS_CAST((klass),				\
+				 HINOKO_TYPE_FW_ISO_RX_MULTIPLE,	\
+				 HinokoFwIsoRxMultipleClass))
+#define HINOKO_IS_FW_ISO_RX_MULTIPLE_CLASS(klass)			\
+	(G_TYPE_CHECK_CLASS_TYPE((klass),				\
+				 HINOKO_TYPE_FW_ISO_RX_MULTIPLE))
+#define HINOKO_FW_ISO_RX_MULTIPLE_GET_CLASS(obj)			\
+	(G_TYPE_INSTANCE_GET_CLASS((obj),				\
+				   HINOKO_TYPE_FW_ISO_RX_MULTIPLE,	\
+				   HinokoFwIsoRxMultipleClass))
+
+typedef struct _HinokoFwIsoRxMultiple		HinokoFwIsoRxMultiple;
+typedef struct _HinokoFwIsoRxMultipleClass	HinokoFwIsoRxMultipleClass;
+
+struct _HinokoFwIsoRxMultiple {
+	HinokoFwIsoCtx parent_instance;
+};
+
+struct _HinokoFwIsoRxMultipleClass {
+	HinokoFwIsoCtxClass parent_class;
+};
+
+GType hinoko_fw_iso_rx_multiple_get_type(void) G_GNUC_CONST;
+
+HinokoFwIsoRxMultiple *hinoko_fw_iso_rx_multiple_new(void);
+
+G_END_DECLS
+
+#endif

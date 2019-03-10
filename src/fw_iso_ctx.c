7168448404f00e174143363e1c003a25e0dbbf31
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-03-11 00:12:13 +0900

    fw_iso_ctx: add an abstract GObject object to maintain isochronous context

---
+++ src/fw_iso_ctx.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "fw_iso_ctx.h"
+
+/**
+ * SECTION:fw_iso_ctx
+ * @Title: HinokoFwIsoCtx
+ * @Short_description: An abstract object to maintain isochronous context.
+ *
+ * A #HinokoFwIsoCtx is an abstract object to maintain isochronous context by
+ * UAPI of Linux FireWire subsystem. All of operations utilize ioctl(2) with
+ * subsystem specific request commands. This object is designed for internal
+ * use, therefore no method is available for applications.
+ */
+G_DEFINE_ABSTRACT_TYPE(HinokoFwIsoCtx, hinoko_fw_iso_ctx, G_TYPE_OBJECT)
+
+static void hinoko_fw_iso_ctx_class_init(HinokoFwIsoCtxClass *klass)
+{
+	return;
+}
+
+static void hinoko_fw_iso_ctx_init(HinokoFwIsoCtx *self)
+{
+	return;
+}
---
+++ src/fw_iso_ctx.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HINOKO_FW_ISO_CTX_H__
+#define __HINOKO_FW_ISO_CTX_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define HINOKO_TYPE_FW_ISO_CTX	(hinoko_fw_iso_ctx_get_type())
+
+#define HINOKO_FW_ISO_CTX(obj)					\
+	(G_TYPE_CHECK_INSTANCE_CAST((obj),			\
+				    HINOKO_TYPE_FW_ISO_CTX,	\
+				    HinokoFwIsoCtx))
+#define HINOKO_IS_FW_ISO_CTX(obj)				\
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj),			\
+				    HINOKO_TYPE_FW_ISO_CTX))
+
+#define HINOKO_FW_ISO_CTX_CLASS(klass)				\
+	(G_TYPE_CHECK_CLASS_CAST((klass),			\
+				 HINOKO_TYPE_FW_ISO_CTX,	\
+				 HinokoFwIsoCtxClass))
+#define HINOKO_IS_FW_ISO_CTX_CLASS(klass)			\
+	(G_TYPE_CHECK_CLASS_TYPE((klass),			\
+				 HINOKO_TYPE_FW_ISO_CTX))
+#define HINOKO_FW_ISO_CTX_GET_CLASS(obj)			\
+	(G_TYPE_INSTANCE_GET_CLASS((obj),			\
+				   HINOKO_TYPE_FW_ISO_CTX,	\
+				   HinokoFwIsoCtxClass))
+
+typedef struct _HinokoFwIsoCtx		HinokoFwIsoCtx;
+typedef struct _HinokoFwIsoCtxClass	HinokoFwIsoCtxClass;
+
+struct _HinokoFwIsoCtx {
+	GObject parent_instance;
+};
+
+struct _HinokoFwIsoCtxClass {
+	GObjectClass parent_class;
+};
+
+GType hinoko_fw_iso_ctx_get_type(void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif

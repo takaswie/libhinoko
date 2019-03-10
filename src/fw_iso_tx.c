4eb942b5f3e4bf57a65d8ee0f5c89b979853ebc1
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-03-11 00:12:13 +0900

    fw_iso_tx: support GQuark

--- src/fw_iso_tx.c
+++ src/fw_iso_tx.c
@@ -13,6 +13,12 @@
  */
 G_DEFINE_TYPE(HinokoFwIsoTx, hinoko_fw_iso_tx, HINOKO_TYPE_FW_ISO_CTX)
 
+// For error handling.
+G_DEFINE_QUARK("HinokoFwIsoTx", hinoko_fw_iso_tx)
+#define raise(exception, errno)					\
+	g_set_error(exception, hinoko_fw_iso_tx_quark(), errno,	\
+		    "%d: %s", __LINE__, strerror(errno))
+
 static void hinoko_fw_iso_tx_class_init(HinokoFwIsoTxClass *klass)
 {
 	return;

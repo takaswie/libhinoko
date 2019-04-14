9d6cd4da18e982ddc1b9308acd4f540b2ce2da9f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-04-14 21:03:49 +0900

    fw_iso_ctx_mode: add enumerators for mode of isochronous context

--- src/hinoko_enum_types.h
+++ src/hinoko_enum_types.h
@@ -3,5 +3,22 @@
 #define __HINOKO_ENUM_TYPES_H__
 
 #include <glib-object.h>
+#include <linux/firewire-cdev.h>
+
+/**
+ * HinokoFwIsoCtxMode:
+ * @HINOKO_FW_ISO_CTX_MODE_TX:		The mode of IT context of 1394 OHCI.
+ * @HINOKO_FW_ISO_CTX_MODE_RX_SINGLE:	The mode of IR context of 1394 OHCI with
+ *					packer-per-buffer protocol
+ * @HINOKO_FW_ISO_CTX_MODE_RX_MULTIPLE: The mode of IR context of 1394 OHCI with
+ *					buffer-fill protocol.
+ *
+ * A representation of mode for isochronous context of Linux FireWire subsystem.
+ */
+typedef enum {
+	HINOKO_FW_ISO_CTX_MODE_TX = FW_CDEV_ISO_CONTEXT_TRANSMIT,
+	HINOKO_FW_ISO_CTX_MODE_RX_SINGLE = FW_CDEV_ISO_CONTEXT_RECEIVE,
+	HINOKO_FW_ISO_CTX_MODE_RX_MULTIPLE = FW_CDEV_ISO_CONTEXT_RECEIVE_MULTICHANNEL,
+} HinokoFwIsoCtxMode;
 
 #endif

e26e4ac9b17cae39ddc664f72ee9ebc2bcdfe4f4
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-12-02 18:33:56 +0900

    cycle_timer: add accessor methods

--- src/cycle_timer.c
+++ src/cycle_timer.c
@@ -27,3 +27,49 @@ HinokoCycleTimer *hinoko_cycle_timer_new()
 {
 	return g_try_malloc0(sizeof(HinokoCycleTimer));
 }
+
+/**
+ * hinoko_cycle_timer_get_timestamp:
+ * @self: A #HinokoCycleTimer.
+ * @tv_sec: (out): The second part of timestamp.
+ * @tv_nsec: (out): The nanosecond part of timestamp.
+ *
+ * Get timestamp with enough sizee of strorage. The timestamp refers to
+ * clock_id available by hinoko_cycle_timer_get_clock_id().
+ */
+void hinoko_cycle_timer_get_timestamp(HinokoCycleTimer *self, gint64 *tv_sec,
+				      gint32 *tv_nsec)
+{
+	*tv_sec = self->tv_sec;
+	*tv_nsec = self->tv_nsec;
+}
+
+/**
+ * hinoko_cycle_timer_get_clock_id:
+ * @self: A #HinokoCycleTimer.
+ * @clock_id: (out): The ID of clock for timestamp, one of HinokoSystemClockId.
+ *
+ * Get the ID of clock for timestamp.
+ */
+void hinoko_cycle_timer_get_clock_id(HinokoCycleTimer *self,
+				     HinokoSystemClockId *clock_id)
+{
+	*clock_id = self->clk_id;
+}
+
+/**
+ * hinoko_cycle_timer_get_cycle_timer:
+ * @self: A #HinokoCycleTimer.
+ * @cycle_timer: (array fixed-size=3)(out caller-allocates): The value of cycle
+ * 		 timer register of 1394 OHCI, including three elements; second,
+ * 		 cycle and offset.
+ *
+ * Get the value of cycle timer in 1394 OHCI controller.
+ */
+void hinoko_cycle_timer_get_cycle_timer(HinokoCycleTimer *self,
+					guint16 cycle_timer[3])
+{
+	cycle_timer[0] = (self->cycle_timer & 0xfe000000) >> 25;
+	cycle_timer[1] = (self->cycle_timer & 0x01fff000) >> 12;
+	cycle_timer[2] = self->cycle_timer & 0x00000fff;
+}
--- src/cycle_timer.h
+++ src/cycle_timer.h
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <hinoko_enums.h>
+
 #include <linux/firewire-cdev.h>
 
 G_BEGIN_DECLS
@@ -16,6 +18,15 @@ typedef struct fw_cdev_get_cycle_timer2 HinokoCycleTimer;
 GType hinoko_cycle_timer_get_type() G_GNUC_CONST;
 
 HinokoCycleTimer *hinoko_cycle_timer_new();
+
+void hinoko_cycle_timer_get_timestamp(HinokoCycleTimer *self, gint64 *tv_sec,
+				      gint32 *tv_nsec);
+
+void hinoko_cycle_timer_get_clock_id(HinokoCycleTimer *self,
+				     HinokoSystemClockId *clock_id);
+
+void hinoko_cycle_timer_get_cycle_timer(HinokoCycleTimer *self,
+					guint16 cycle_timer[3]);
 
 G_END_DECLS
 

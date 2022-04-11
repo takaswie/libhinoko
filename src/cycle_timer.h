takaswie/libhinoko

commit 37625a1dac0cd906a5a51124a0199ce8773c16c4

cycle_timer: add a boxed object to represent cycle timer structure

src/cycle_timer.h
@@ -1,0 +1,22 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HINOKO_CYCLE_TIMER__H__
+#define __HINOKO_CYCLE_TIMER__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <linux/firewire-cdev.h>
+
+G_BEGIN_DECLS
+
+#define HINOKO_TYPE_CYCLE_TIMER	(hinoko_cycle_timer_get_type())
+
+typedef struct fw_cdev_get_cycle_timer2 HinokoCycleTimer;
+
+GType hinoko_cycle_timer_get_type() G_GNUC_CONST;
+
+HinokoCycleTimer *hinoko_cycle_timer_new();
+
+G_END_DECLS
+
+#endif

src/cycle_timer.c
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "cycle_timer.h"
+
+/**
+ * SECTION:cycle_timer
+ * @Title: HinokoCycleTimer
+ * @Short_description: A boxed object to represent data for cycle timer.
+ *
+ * A #HinokoCycleTimer is an boxed object to represent the value of cycle
+ * timer and timestamp referring to clock_id.
+ */
+HinokoCycleTimer *hinoko_cycle_timer_copy(const HinokoCycleTimer *self)
+{
+	return g_memdup(self, sizeof(*self));
+}
+
+G_DEFINE_BOXED_TYPE(HinokoCycleTimer, hinoko_cycle_timer, hinoko_cycle_timer_copy, g_free)
+
+/**
+ * hinoko_cycle_timer_new:
+ *
+ * Allocate and return an instance of HinokoCycleTimer.
+ *
+ * Returns: (transfer none): An instance of HinokoCycleTimer.
+ */
+HinokoCycleTimer *hinoko_cycle_timer_new()
+{
+	return g_try_malloc0(sizeof(HinokoCycleTimer));
+}

src/hinoko.map
@@ -49,4 +49,7 @@ HINOKO_0_1_0 {
 HINOKO_0_3_0 {
   global:
     "hinoko_system_clock_id_get_type";
+
+    "hinoko_cycle_timer_get_type";
+    "hinoko_cycle_timer_new";
 } HINOKO_0_1_0;

src/meson.build
@@ -13,6 +13,7 @@ sources = [
   'fw_iso_rx_single.c',
   'fw_iso_rx_multiple.c',
   'fw_iso_tx.c',
+  'cycle_timer.c',
 ]
 
 headers = [
@@ -20,6 +21,7 @@ headers = [
   'fw_iso_rx_single.h',
   'fw_iso_rx_multiple.h',
   'fw_iso_tx.h',
+  'cycle_timer.h',
 ]
 
 # Generate marshallers for GObject signals.

commit 4cab3bc90a1d01e94622233467eb38ac8dbc61ec

cycle_timer: minor code refactoring for inclusion guard
Fixes: 37625a1dac0c ("cycle_timer: add a boxed object to represent cycle timer structure")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/cycle_timer.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-#ifndef __HINOKO_CYCLE_TIMER__H__
-#define __HINOKO_CYCLE_TIMER__H__
+#ifndef __HINOKO_CYCLE_TIMER_H__
+#define __HINOKO_CYCLE_TIMER_H__
 
 #include <hinoko.h>
 
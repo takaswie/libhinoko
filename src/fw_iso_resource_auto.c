takaswie/libhinoko

commit 4905daafa2fabf00eba1e2186b31534edc6b6728

fw_iso_resource_auto: add GObject object for maintainer of isochronous resource

src/fw_iso_resource_auto.c
@@ -1,0 +1,26 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "fw_iso_resource_auto.h"
+
+G_DEFINE_TYPE(HinokoFwIsoResourceAuto, hinoko_fw_iso_resource_auto, HINOKO_TYPE_FW_ISO_RESOURCE)
+
+static void hinoko_fw_iso_resource_auto_class_init(HinokoFwIsoResourceAutoClass *klass)
+{
+	return;
+}
+
+static void hinoko_fw_iso_resource_auto_init(HinokoFwIsoResourceAuto *self)
+{
+	return;
+}
+
+/**
+ * hinoko_fw_iso_resource_auto:
+ *
+ * Allocate and return an instance of #HinokoFwIsoResourceAuto object.
+ *
+ * Returns: A #HinokoFwIsoResourceAuto.
+ */
+HinokoFwIsoResourceAuto *hinoko_fw_iso_resource_auto_new()
+{
+	return g_object_new(HINOKO_TYPE_FW_ISO_RESOURCE_AUTO, NULL);
+}

src/fw_iso_resource_auto.h
@@ -1,0 +1,49 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HINOKO_FW_ISO_RESOURCE_AUTO_AUTO_H__
+#define __HINOKO_FW_ISO_RESOURCE_AUTO_AUTO_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <fw_iso_resource.h>
+
+G_BEGIN_DECLS
+
+#define HINOKO_TYPE_FW_ISO_RESOURCE_AUTO	(hinoko_fw_iso_resource_auto_get_type())
+
+#define HINOKO_FW_ISO_RESOURCE_AUTO(obj)				\
+	(G_TYPE_CHECK_INSTANCE_CAST((obj),				\
+				    HINOKO_TYPE_FW_ISO_RESOURCE_AUTO,	\
+				    HinokoFwIsoResourceAuto))
+#define HINOKO_IS_FW_ISO_RESOURCE_AUTO(obj)				\
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj),				\
+				    HINOKO_TYPE_FW_ISO_RESOURCE_AUTO))
+
+#define HINOKO_FW_ISO_RESOURCE_AUTO_CLASS(klass)			\
+	(G_TYPE_CHECK_CLASS_CAST((klass),				\
+				 HINOKO_TYPE_FW_ISO_RESOURCE_AUTO,	\
+				 HinokoFwIsoResourceAutoClass))
+#define HINOKO_IS_FW_ISO_RESOURCE_AUTO_CLASS(klass)			\
+	(G_TYPE_CHECK_CLASS_TYPE((klass),				\
+				 HINOKO_TYPE_FW_ISO_RESOURCE_AUTO))
+#define HINOKO_FW_ISO_RESOURCE_AUTO_GET_CLASS(obj)			\
+	(G_TYPE_INSTANCE_GET_CLASS((obj),				\
+				   HINOKO_TYPE_FW_ISO_RESOURCE_AUTO,	\
+				   HinokoFwIsoResourceAutoClass))
+
+typedef struct _HinokoFwIsoResourceAuto		HinokoFwIsoResourceAuto;
+typedef struct _HinokoFwIsoResourceAutoClass	HinokoFwIsoResourceAutoClass;
+
+struct _HinokoFwIsoResourceAuto {
+	HinokoFwIsoResource parent_instance;
+};
+
+struct _HinokoFwIsoResourceAutoClass {
+	HinokoFwIsoResourceClass parent_class;
+};
+
+GType hinoko_fw_iso_resource_auto_get_type(void) G_GNUC_CONST;
+
+HinokoFwIsoResourceAuto *hinoko_fw_iso_resource_auto_new();
+
+#endif

src/hinoko.map
@@ -64,4 +64,7 @@ HINOKO_0_4_0 {
     "hinoko_fw_iso_resource_deallocate_once_async";
     "hinoko_fw_iso_resource_allocate_once_sync";
     "hinoko_fw_iso_resource_deallocate_once_sync";
+
+    "hinoko_fw_iso_resource_auto_get_type";
+    "hinoko_fw_iso_resource_auto_new";
 } HINOKO_0_3_0;

src/meson.build
@@ -15,6 +15,7 @@ sources = [
   'fw_iso_tx.c',
   'cycle_timer.c',
   'fw_iso_resource.c',
+  'fw_iso_resource_auto.c',
 ]
 
 headers = [
@@ -24,6 +25,7 @@ headers = [
   'fw_iso_tx.h',
   'cycle_timer.h',
   'fw_iso_resource.h',
+  'fw_iso_resource_auto.h',
 ]
 
 # Generate marshallers for GObject signals.

commit 9fb9c815650ad73edca136f676855ceed1a6c6b2

fw_iso_resource_auto: fix invalid annotation for function name
Fixes: 4905daafa2fa ("fw_iso_resource_auto: add GObject object for maintainer of isochronous resource")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_resource_auto.c
@@ -118,7 +118,7 @@ static void hinoko_fw_iso_resource_auto_init(HinokoFwIsoResourceAuto *self)
 }
 
 /**
- * hinoko_fw_iso_resource_auto:
+ * hinoko_fw_iso_resource_auto_new:
  *
  * Allocate and return an instance of #HinokoFwIsoResourceAuto object.
  *
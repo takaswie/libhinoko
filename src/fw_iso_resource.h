takaswie/libhinoko

commit da70bafea319a6caefff932fdf7ae97c7305d084

fw_iso_resource: add GObject object for lister of isochronous resource allocation

src/fw_iso_resource.h
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HINOKO_FW_ISO_RESOURCE_H__
+#define __HINOKO_FW_ISO_RESOURCE_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define HINOKO_TYPE_FW_ISO_RESOURCE	(hinoko_fw_iso_resource_get_type())
+
+#define HINOKO_FW_ISO_RESOURCE(obj)					\
+	(G_TYPE_CHECK_INSTANCE_CAST((obj),				\
+					HINOKO_TYPE_FW_ISO_RESOURCE,	\
+					HinokoFwIsoResource))
+#define HINOKO_IS_FW_ISO_RESOURCE(obj)					\
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj),				\
+				    HINOKO_TYPE_FW_ISO_RESOURCE))
+
+#define HINOKO_FW_ISO_RESOURCE_CLASS(klass)				\
+	(G_TYPE_CHECK_CLASS_CAST((klass),				\
+				 HINOKO_TYPE_FW_ISO_RESOURCE,		\
+				 HinokoFwIsoResourceClass))
+#define HINOKO_IS_FW_ISO_RESOURCE_CLASS(klass)				\
+	(G_TYPE_CHECK_CLASS_TYPE((klass),				\
+				 HINOKO_TYPE_FW_ISO_RESOURCE))
+#define HINOKO_FW_ISO_RESOURCE_GET_CLASS(obj)				\
+	(G_TYPE_INSTANCE_GET_CLASS((obj),				\
+				   HINOKO_TYPE_FW_ISO_RESOURCE,		\
+				   HinokoFwIsoResourceClass))
+
+typedef struct _HinokoFwIsoResource		HinokoFwIsoResource;
+typedef struct _HinokoFwIsoResourceClass	HinokoFwIsoResourceClass;
+
+struct _HinokoFwIsoResource {
+	GObject parent_instance;
+};
+
+struct _HinokoFwIsoResourceClass {
+	GObjectClass parent_class;
+};
+
+GType hinoko_fw_iso_resource_get_type(void) G_GNUC_CONST;
+
+HinokoFwIsoResource *hinoko_fw_iso_resource_new();
+
+#endif

src/fw_iso_resource.c
@@ -1,0 +1,26 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "fw_iso_resource.h"
+
+G_DEFINE_TYPE(HinokoFwIsoResource, hinoko_fw_iso_resource, G_TYPE_OBJECT)
+
+static void hinoko_fw_iso_resource_class_init(HinokoFwIsoResourceClass *klass)
+{
+	return;
+}
+
+static void hinoko_fw_iso_resource_init(HinokoFwIsoResource *self)
+{
+	return;
+}
+
+/**
+ * hinoko_fw_iso_resource_new:
+ *
+ * Allocate and return an instance of #HinokoFwIsoResource.
+ *
+ * Returns: A #HinokoFwIsoResource.
+ */
+HinokoFwIsoResource *hinoko_fw_iso_resource_new()
+{
+	return g_object_new(HINOKO_TYPE_FW_ISO_RESOURCE, NULL);
+}

src/hinoko.map
@@ -52,3 +52,9 @@ HINOKO_0_3_0 {
     "hinoko_cycle_timer_get_clock_id";
     "hinoko_cycle_timer_get_cycle_timer";
 } HINOKO_0_1_0;
+
+HINOKO_0_4_0 {
+  global:
+    "hinoko_fw_iso_resource_get_type";
+    "hinoko_fw_iso_resource_new";
+} HINOKO_0_3_0;

src/meson.build
@@ -14,6 +14,7 @@ sources = [
   'fw_iso_rx_multiple.c',
   'fw_iso_tx.c',
   'cycle_timer.c',
+  'fw_iso_resource.c',
 ]
 
 headers = [
@@ -22,6 +23,7 @@ headers = [
   'fw_iso_rx_multiple.h',
   'fw_iso_tx.h',
   'cycle_timer.h',
+  'fw_iso_resource.h',
 ]
 
 # Generate marshallers for GObject signals.

commit 57479a5ee7e0fb3321b6fd12250c7994420546a9

fw_iso_resource: fix missing macro for the end of declaration
I forgot it.

Fixes: da70bafea319 ("fw_iso_resource: add GObject object for lister of isochronous resource allocation")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_resource.h
@@ -82,4 +82,6 @@ void hinoko_fw_iso_resource_deallocate_once_sync(HinokoFwIsoResource *self,
 						 guint bandwidth,
 						 GError **exception);
 
+G_END_DECLS
+
 #endif
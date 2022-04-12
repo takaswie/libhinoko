takaswie/libhinoko

commit 5bfaa7445bcda347d999b34c460b1b9460650eb3

fw_iso_ctx: add 'stopped' GObject signal to report stop of isochronous context

src/fw_iso_ctx.h
@@ -43,6 +43,16 @@ struct _HinokoFwIsoCtx {
 
 struct _HinokoFwIsoCtxClass {
 	GObjectClass parent_class;
+
+	/**
+	 * HinokoFwIsoCtxClass::stopped:
+	 * @self: A #HinokoFwIsoCtx.
+	 * @error: (nullable): A #GError.
+	 *
+	 * When isochronous context is stopped, #HinokoFwIsoCtxClass::stopped
+	 * handler is called. When any error occurs, it's reported.
+	 */
+	void (*stopped)(HinokoFwIsoCtx *self, GError *exception);
 };
 
 GType hinoko_fw_iso_ctx_get_type(void) G_GNUC_CONST;

samples/iso-tx
@@ -27,9 +27,11 @@ class IsoTx(Hinoko.FwIsoTx):
         self.unmap_buffer()
         self.release()
 
-    def begin(self, dispatcher):
+    def begin(self, dispatcher, timer):
         self.__src = self.create_source()
         self.__src.attach(dispatcher.get_context())
+        self.__dispatcher = dispatcher
+        self.__timer = timer
 
         self.tag = Hinoko.FwIsoCtxMatchFlag.TAG1
         self.sy = 0
@@ -41,10 +43,17 @@ class IsoTx(Hinoko.FwIsoTx):
                                  irq)
 
         self.start(None)
+        self.__timer.start()
 
     def finish(self):
         self.__src.destroy()
         self.stop()
+
+    def do_stopped(self, error):
+        self.__dispatcher.quit()
+        if error:
+            self.__timer.cancel()
+            print(error)
 
     def do_interrupted(self, sec, cycle, header, header_length, count):
         headers = unpack('>{0}I'.format(header_length // 4), header)
@@ -73,9 +82,9 @@ ctx.new('/dev/fw0', channel, maximum_bytes_per_payload)
 dispatcher = GLib.MainLoop.new(None, False)
 
 # Finish event loop after 4 seconds.
-Timer(4, lambda dispatcher: dispatcher.quit(), args=(dispatcher, )).start()
+timer = Timer(4, lambda dispatcher: dispatcher.quit(), args=(dispatcher, ))
 
-ctx.begin(dispatcher)
+ctx.begin(dispatcher, timer)
 dispatcher.run()
 ctx.finish()
 ctx.destroy()

src/fw_iso_ctx.c
@@ -149,7 +149,7 @@ static void hinoko_fw_iso_ctx_class_init(HinokoFwIsoCtxClass *klass)
 		g_signal_new("stopped",
 			G_OBJECT_CLASS_TYPE(klass),
 			G_SIGNAL_RUN_LAST,
-			0,
+			G_STRUCT_OFFSET(HinokoFwIsoCtxClass, stopped),
 			NULL, NULL,
 			g_cclosure_marshal_VOID__BOXED,
 			G_TYPE_NONE, 1, G_TYPE_ERROR);

tests/fw-iso-ctx
@@ -19,7 +19,9 @@ methods = (
     'get_cycle_timer',
     'create_source',
 )
-vmethods = ()
+vmethods = (
+    'do_stopped',
+)
 signals = (
     'stopped',
 )

commit 4c66c59c9762635a5bcbedfafa593acfd9f8958a

fw_iso_ctx: make error argument of signal as constant
The 'error' argument of Hinoko.FwIsoCtx.stopped signal is immutable.

Fixes: 5bfaa7445bcd ("fw_iso_ctx: add 'stopped' GObject signal to report stop of isochronous context")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_ctx.h
@@ -25,7 +25,7 @@ struct _HinokoFwIsoCtxClass {
 	 * When isochronous context is stopped, #HinokoFwIsoCtxClass::stopped
 	 * handler is called. When any error occurs, it's reported.
 	 */
-	void (*stopped)(HinokoFwIsoCtx *self, GError *error);
+	void (*stopped)(HinokoFwIsoCtx *self, const GError *error);
 };
 
 
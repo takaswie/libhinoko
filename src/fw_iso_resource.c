takaswie/libhinoko

commit ba2ccbe77cfa95dee154bdd385f6f458695d90c5

fw_iso_resource: add GObject signal for allocation and deallocation

src/fw_iso_resource.c
@@ -20,6 +20,13 @@ typedef struct {
 	guint8 *buf;
 } FwIsoResourceSource;
 
+enum fw_iso_resource_sig_type {
+	FW_ISO_RESOURCE_SIG_ALLOCATED = 0,
+	FW_ISO_RESOURCE_SIG_DEALLOCATED,
+	FW_ISO_RESOURCE_SIG_COUNT,
+};
+static guint fw_iso_resource_sigs[FW_ISO_RESOURCE_SIG_COUNT] = { 0 };
+
 static void fw_iso_resource_finalize(GObject *obj)
 {
 	HinokoFwIsoResource *self = HINOKO_FW_ISO_RESOURCE(obj);
@@ -37,6 +44,46 @@ static void hinoko_fw_iso_resource_class_init(HinokoFwIsoResourceClass *klass)
 	GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
 
 	gobject_class->finalize = fw_iso_resource_finalize;
+
+	/**
+	 * HinokoFwIsoResource::allocated:
+	 * @self: A #HinokoFwIsoResource.
+	 * @channel: The deallocated channel number.
+	 * @bandwidth: The deallocated amount of bandwidth.
+	 * @err_code: 0 if successful, else any error code.
+	 *
+	 * When allocation of isochronous resource finishes, the ::allocated
+	 * handler is called to notify the result, channel, and bandwidth.
+	 */
+	fw_iso_resource_sigs[FW_ISO_RESOURCE_SIG_ALLOCATED] =
+		g_signal_new("allocated",
+			     G_OBJECT_CLASS_TYPE(klass),
+			     G_SIGNAL_RUN_LAST,
+			     0,
+			     NULL, NULL,
+			     hinoko_sigs_marshal_VOID__UINT_UINT_UINT,
+			     G_TYPE_NONE,
+			     3, G_TYPE_UINT, G_TYPE_UINT, G_TYPE_UINT);
+
+	/**
+	 * HinokoFwIsoResource::deallocated:
+	 * @self: A #HinokoFwIsoResource.
+	 * @channel: The deallocated channel number.
+	 * @bandwidth: The deallocated amount of bandwidth.
+	 * @err_code: 0 if successful, else any error code.
+	 *
+	 * When deallocation of isochronous resource finishes, the ::deallocated
+	 * handler is called to notify the result, channel, and bandwidth.
+	 */
+	fw_iso_resource_sigs[FW_ISO_RESOURCE_SIG_DEALLOCATED] =
+		g_signal_new("deallocated",
+			     G_OBJECT_CLASS_TYPE(klass),
+			     G_SIGNAL_RUN_LAST,
+			     0,
+			     NULL, NULL,
+			     hinoko_sigs_marshal_VOID__UINT_UINT_UINT,
+			     G_TYPE_NONE,
+			     3, G_TYPE_UINT, G_TYPE_UINT, G_TYPE_UINT);
 }
 
 static void hinoko_fw_iso_resource_init(HinokoFwIsoResource *self)
@@ -83,6 +130,33 @@ void hinoko_fw_iso_resource_open(HinokoFwIsoResource *self, const gchar *path,
 		generate_error(exception, errno);
 }
 
+static void handle_iso_resource_event(HinokoFwIsoResource *self,
+				      struct fw_cdev_event_iso_resource *ev)
+{
+	guint channel;
+	guint bandwidth;
+	guint err_code;
+	int sig_type;
+
+	if (ev->channel >= 0) {
+		channel = ev->channel;
+		bandwidth = ev->bandwidth;
+		err_code = 0;
+	} else {
+		channel = 0;
+		bandwidth = 0;
+		err_code = -ev->channel;
+	}
+
+	if (ev->type == FW_CDEV_EVENT_ISO_RESOURCE_ALLOCATED)
+		sig_type = FW_ISO_RESOURCE_SIG_ALLOCATED;
+	else
+		sig_type = FW_ISO_RESOURCE_SIG_DEALLOCATED;
+
+	g_signal_emit(self, fw_iso_resource_sigs[sig_type],
+		      0, channel, bandwidth, err_code);
+}
+
 static gboolean check_src(GSource *gsrc)
 {
 	FwIsoResourceSource *src = (FwIsoResourceSource *)gsrc;
@@ -102,6 +176,7 @@ static gboolean dispatch_src(GSource *gsrc, GSourceFunc cb, gpointer user_data)
 			hinoko_fw_iso_resource_get_instance_private(self);
 	GIOCondition condition;
 	ssize_t len;
+	guint8 *buf;
 
 	if (priv->fd < 0)
 		return G_SOURCE_REMOVE;
@@ -118,7 +193,25 @@ static gboolean dispatch_src(GSource *gsrc, GSourceFunc cb, gpointer user_data)
 		return G_SOURCE_REMOVE;
 	}
 
-	// TODO: dispatch the events.
+	buf = src->buf;
+	while (len > 0) {
+		union fw_cdev_event *ev = (union fw_cdev_event *)buf;
+		size_t size = 0;
+
+		switch (ev->common.type) {
+		case FW_CDEV_EVENT_ISO_RESOURCE_ALLOCATED:
+		case FW_CDEV_EVENT_ISO_RESOURCE_DEALLOCATED:
+			handle_iso_resource_event(self, &ev->iso_resource);
+			size = sizeof(ev->iso_resource);
+			break;
+		default:
+			break;
+		}
+
+		len -= size;
+		buf += size;
+	}
+
 
 	// Just be sure to continue to process this source.
 	return G_SOURCE_CONTINUE;

src/fw_iso_resource.h
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <hinoko_sigs_marshal.h>
+
 G_BEGIN_DECLS
 
 #define HINOKO_TYPE_FW_ISO_RESOURCE	(hinoko_fw_iso_resource_get_type())

src/hinoko_sigs_marshal.list
@@ -1,1 +1,2 @@
 VOID:UINT,UINT,POINTER,UINT,UINT
+VOID:UINT,UINT,UINT

tests/fw-iso-resource
@@ -17,7 +17,10 @@ methods = (
     'create_source',
 )
 vmethods = ()
-signals = ()
+signals = (
+    'allocated',
+    'deallocated',
+)
 
 if not test(target, props, methods, vmethods, signals):
     exit(ENXIO)

commit d637d28c29e88146454792f3422383d82d5a4f30

fw_iso_resource: fix GError override warning at error case
Unless local variable is not NULL, g_set_error() generates warning.

This commit fixes the bug.

Fixes: ba2ccbe77cfa ("fw_iso_resource: add GObject signal for allocation and deallocation")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_resource.c
@@ -94,8 +94,8 @@ static void hinoko_fw_iso_resource_class_init(HinokoFwIsoResourceClass *klass)
 	 * @self: A [class@FwIsoResource].
 	 * @channel: The deallocated channel number.
 	 * @bandwidth: The deallocated amount of bandwidth.
-	 * @error: A [struct@GLib.Error]. Error can be generated with domain of
-	 *	   Hinoko.FwIsoResourceError and its EVENT code.
+	 * @error: (transfer none) (nullable): A [struct@GLib.Error]. Error can be generated with
+	 *	   domain of Hinoko.FwIsoResourceError and its EVENT code.
 	 *
 	 * Emitted when allocation of isochronous resource finishes.
 	 */
@@ -114,8 +114,8 @@ static void hinoko_fw_iso_resource_class_init(HinokoFwIsoResourceClass *klass)
 	 * @self: A [class@FwIsoResource].
 	 * @channel: The deallocated channel number.
 	 * @bandwidth: The deallocated amount of bandwidth.
-	 * @error: A [struct@GLib.Error]. Error can be generated with domain of
-	 *	   Hinoko.FwIsoResourceError and its EVENT code.
+	 * @error: (transfer none) (nullable): A [struct@GLib.Error]. Error can be generated with
+	 *	   domain of Hinoko.FwIsoResourceError and its EVENT code.
 	 *
 	 * Emitted when deallocation of isochronous resource finishes.
 	 */
@@ -447,7 +447,7 @@ static void handle_iso_resource_event(HinokoFwIsoResource *self,
 	guint channel;
 	guint bandwidth;
 	int sig_type;
-	GError *error;
+	GError *error = NULL;
 
 	// To change state machine for auto mode.
 	if (HINOKO_IS_FW_ISO_RESOURCE_AUTO(self)) {
@@ -458,7 +458,6 @@ static void handle_iso_resource_event(HinokoFwIsoResource *self,
 	if (ev->channel >= 0) {
 		channel = ev->channel;
 		bandwidth = ev->bandwidth;
-		error = NULL;
 	} else {
 		channel = 0;
 		bandwidth = 0;

src/fw_iso_resource.h
@@ -23,8 +23,8 @@ struct _HinokoFwIsoResourceClass {
 	 * @self: A [class@FwIsoResource].
 	 * @channel: The deallocated channel number.
 	 * @bandwidth: The deallocated amount of bandwidth.
-	 * @error: A [struct@GLib.Error]. Error can be generated with domain of
-	 *	   Hinoko.FwIsoResourceError and its EVENT code.
+	 * @error: (transfer none) (nullable): A [struct@GLib.Error]. Error can be generated with
+	 *	   domain of Hinoko.FwIsoResourceError and its EVENT code.
 	 *
 	 * Class closure for the [signal@FwIsoResource::allocated] signal.
 	 */
@@ -36,8 +36,8 @@ struct _HinokoFwIsoResourceClass {
 	 * @self: A [class@FwIsoResource].
 	 * @channel: The deallocated channel number.
 	 * @bandwidth: The deallocated amount of bandwidth.
-	 * @error: A [struct@GLib.Error]. Error can be generated with domain of
-	 *	   Hinoko.FwIsoResourceError and its EVENT code.
+	 * @error: (transfer none) (nullable): A [struct@GLib.Error]. Error can be generated with
+	 *	   domain of Hinoko.FwIsoResourceError and its EVENT code.
 	 *
 	 * Class closure for the [signal@FwIsoResource::deallocated] signal.
 	 */
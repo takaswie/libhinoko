7a6eb8fd2ffc4834f792dd5fcf9f901580f77170
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-05 21:15:33 +0900

    fw_iso_tx: rewrite public API to return gboolean according to GNOME convention
    In GNOME convention, the throw function to report error at GError argument
    should return gboolean value to report the overall operation finishes
    successfully or not. On the other hand, it's implemented to return void
    in Hinoko.FwIsoTx.
    
    This commit rewrite such public APIs with loss of backward compatibility.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/fw_iso_tx.h
+++ src/fw_iso_tx.h
@@ -32,22 +32,19 @@ struct _HinokoFwIsoTxClass {
 
 HinokoFwIsoTx *hinoko_fw_iso_tx_new(void);
 
-void hinoko_fw_iso_tx_allocate(HinokoFwIsoTx *self, const char *path,
-			       HinokoFwScode scode, guint channel,
-			       guint header_size, GError **error);
+gboolean hinoko_fw_iso_tx_allocate(HinokoFwIsoTx *self, const char *path, HinokoFwScode scode,
+				   guint channel, guint header_size, GError **error);
 
-void hinoko_fw_iso_tx_map_buffer(HinokoFwIsoTx *self,
-				 guint maximum_bytes_per_payload,
-				 guint payloads_per_buffer,
-				 GError **error);
+gboolean hinoko_fw_iso_tx_map_buffer(HinokoFwIsoTx *self, guint maximum_bytes_per_payload,
+				     guint payloads_per_buffer, GError **error);
 
-void hinoko_fw_iso_tx_start(HinokoFwIsoTx *self, const guint16 *cycle_match, GError **error);
+gboolean hinoko_fw_iso_tx_start(HinokoFwIsoTx *self, const guint16 *cycle_match, GError **error);
 
-void hinoko_fw_iso_tx_register_packet(HinokoFwIsoTx *self,
-				HinokoFwIsoCtxMatchFlag tags, guint sy,
-				const guint8 *header, guint header_length,
-				const guint8 *payload, guint payload_length,
-				gboolean schedule_interrupt, GError **error);
+gboolean hinoko_fw_iso_tx_register_packet(HinokoFwIsoTx *self, HinokoFwIsoCtxMatchFlag tags,
+					  guint sy,
+					  const guint8 *header, guint header_length,
+					  const guint8 *payload, guint payload_length,
+					  gboolean schedule_interrupt, GError **error);
 
 G_END_DECLS
 

takaswie/libhinoko

commit 993deba69909309a476abed1d0d44357972d418e

add entry header to aggregate headers
It's convenient to add header to include all of headers in regard of
documentation.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinoko_enum_types.h
@@ -2,9 +2,6 @@
 #ifndef __HINOKO_ENUM_TYPES_H__
 #define __HINOKO_ENUM_TYPES_H__
 
-#include <glib-object.h>
-#include <linux/firewire-cdev.h>
-
 /**
  * HinokoFwIsoCtxMode:
  * @HINOKO_FW_ISO_CTX_MODE_TX:		The mode of IT context of 1394 OHCI.

src/cycle_timer.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-#include "cycle_timer.h"
+#include "internal.h"
 
 /**
  * SECTION:cycle_timer

src/cycle_timer.h
@@ -2,12 +2,7 @@
 #ifndef __HINOKO_CYCLE_TIMER__H__
 #define __HINOKO_CYCLE_TIMER__H__
 
-#include <glib.h>
-#include <glib-object.h>
-
-#include <hinoko_enums.h>
-
-#include <linux/firewire-cdev.h>
+#include <hinoko.h>
 
 G_BEGIN_DECLS
 

src/fw_iso_ctx.c
@@ -8,7 +8,6 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <errno.h>
-#include <linux/firewire-cdev.h>
 
 /**
  * SECTION:fw_iso_ctx

src/fw_iso_ctx.h
@@ -2,11 +2,7 @@
 #ifndef __HINOKO_FW_ISO_CTX_H__
 #define __HINOKO_FW_ISO_CTX_H__
 
-#include <glib.h>
-#include <glib-object.h>
-
-#include <cycle_timer.h>
-#include <hinoko_enums.h>
+#include <hinoko.h>
 
 G_BEGIN_DECLS
 

src/fw_iso_resource.c
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+#include "internal.h"
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-
-#include "internal.h"
-#include "hinoko_sigs_marshal.h"
 
 /**
  * SECTION:fw_iso_resource

src/fw_iso_resource.h
@@ -2,10 +2,7 @@
 #ifndef __HINOKO_FW_ISO_RESOURCE_H__
 #define __HINOKO_FW_ISO_RESOURCE_H__
 
-#include <glib.h>
-#include <glib-object.h>
-
-#include <hinoko_enums.h>
+#include <hinoko.h>
 
 G_BEGIN_DECLS
 

src/fw_iso_resource_auto.c
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-#include <errno.h>
-
 #include "internal.h"
 
-#include <linux/firewire-cdev.h>
+#include <errno.h>
 
 /**
  * SECTION:fw_iso_resource_auto

commit 95d8e0cf19ecb995b48a08635eab3e29dad5a9a9

fix missing usage of declaration macro
Fixes: 993deba69909 ("add entry header to aggregate headers")
Fixes: 6a5b484f47e4 ("meson: add support GLib enumerations")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinoko_enum_types.h
@@ -2,6 +2,8 @@
 #ifndef __HINOKO_ENUM_TYPES_H__
 #define __HINOKO_ENUM_TYPES_H__
 
+G_BEGIN_DECLS
+
 /**
  * HinokoFwIsoCtxMode:
  * @HINOKO_FW_ISO_CTX_MODE_TX:		The mode of IT context of 1394 OHCI.
@@ -116,5 +118,7 @@ typedef enum {
 	HINOKO_FW_ISO_CTX_ERROR_CHUNK_UNREGISTERED,
 	HINOKO_FW_ISO_CTX_ERROR_NO_ISOC_CHANNEL,
 } HinokoFwIsoCtxError;
+
+G_END_DECLS
 
 #endif
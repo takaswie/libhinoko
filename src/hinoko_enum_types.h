takaswie/libhinoko

commit 74854652dce92207f4e88080f2b980e14630bf63

fw_scode: add the enumerators for speed on IEEE 1394 bus

src/hinoko_enum_types.h
@@ -21,4 +21,26 @@ typedef enum {
 	HINOKO_FW_ISO_CTX_MODE_RX_MULTIPLE = FW_CDEV_ISO_CONTEXT_RECEIVE_MULTICHANNEL,
 } HinokoFwIsoCtxMode;
 
+/**
+ * HinokoFwScode:
+ * @HINOKO_FW_SCODE_S100:	100 bps.
+ * @HINOKO_FW_SCODE_S200:	200 bps.
+ * @HINOKO_FW_SCODE_S400:	400 bps.
+ * @HINOKO_FW_SCODE_S800:	800 bps.
+ * @HINOKO_FW_SCODE_S1600:	1600 bps.
+ * @HINOKO_FW_SCODE_S3200:	3200 bps.
+ * @HINOKO_FW_SCODE_BETA:	The speed defined in beta specification.
+ *
+ * A representation of speed for isochronous context on IEEE 1394 bus.
+ */
+typedef enum {
+	HINOKO_FW_SCODE_S100	= SCODE_100,
+	HINOKO_FW_SCODE_S200	= SCODE_200,
+	HINOKO_FW_SCODE_S400	= SCODE_400,
+	HINOKO_FW_SCODE_S800	= SCODE_800,
+	HINOKO_FW_SCODE_S1600	= SCODE_1600,
+	HINOKO_FW_SCODE_S3200	= SCODE_3200,
+	HINOKO_FW_SCODE_BETA	= SCODE_BETA,
+} HinokoFwScode;
+
 #endif

src/hinoko.map
@@ -1,6 +1,7 @@
 HINOKO_0_1_0 {
   global:
     "hinoko_fw_iso_ctx_mode_get_type";
+    "hinoko_fw_scode_get_type";
   local:
     *;
 };

commit 754eb5fff83bbd78570fff306ecfc6f2f8a9a0ca

enums: fix speed unit
Fixes: 74854652dce9 ("fw_scode: add the enumerators for speed on IEEE 1394 bus")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinoko_enum_types.h
@@ -22,12 +22,12 @@ typedef enum {
 
 /**
  * HinokoFwScode:
- * @HINOKO_FW_SCODE_S100:	100 bps.
- * @HINOKO_FW_SCODE_S200:	200 bps.
- * @HINOKO_FW_SCODE_S400:	400 bps.
- * @HINOKO_FW_SCODE_S800:	800 bps.
- * @HINOKO_FW_SCODE_S1600:	1600 bps.
- * @HINOKO_FW_SCODE_S3200:	3200 bps.
+ * @HINOKO_FW_SCODE_S100:	100 Mbps.
+ * @HINOKO_FW_SCODE_S200:	200 Mbps.
+ * @HINOKO_FW_SCODE_S400:	400 Mbps.
+ * @HINOKO_FW_SCODE_S800:	800 Mbps.
+ * @HINOKO_FW_SCODE_S1600:	1600 Mbps.
+ * @HINOKO_FW_SCODE_S3200:	3200 Mbps.
  *
  * A representation of speed for isochronous context on IEEE 1394 bus.
  */
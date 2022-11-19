takaswie/libhinoko

commit ba857a847170eaad6a97b533244f2576bcac0393

fw_iso_rx_single: add APIs to map/unmap packet buffer

src/fw_iso_rx_single.c
@@ -103,5 +103,44 @@ void hinoko_fw_iso_rx_single_release(HinokoFwIsoRxSingle *self)
 {
 	g_return_if_fail(HINOKO_IS_FW_ISO_RX_SINGLE(self));
 
+	hinoko_fw_iso_rx_single_unmap_buffer(self);
 	hinoko_fw_iso_ctx_release(HINOKO_FW_ISO_CTX(self));
+}
+
+/*
+ * hinoko_fw_iso_rx_single_map_buffer:
+ * @self: A #HinokoFwIsoRxSingle.
+ * @maximum_bytes_per_payload: The maximum number of bytes per payload of IR
+ *			       context.
+ * @payloads_per_buffer: The number of payload in buffer.
+ * @payloads_per_irq: The number of payload per interval of interrupt.
+ * @exception: A #GError.
+ *
+ * Map intermediate buffer to share payload of IR context with 1394 OHCI
+ * controller.
+ */
+void hinoko_fw_iso_rx_single_map_buffer(HinokoFwIsoRxSingle *self,
+					guint maximum_bytes_per_payload,
+					guint payloads_per_buffer,
+					GError **exception)
+{
+	g_return_if_fail(HINOKO_IS_FW_ISO_RX_SINGLE(self));
+
+	hinoko_fw_iso_ctx_map_buffer(HINOKO_FW_ISO_CTX(self),
+				     maximum_bytes_per_payload,
+				     payloads_per_buffer, exception);
+}
+
+/**
+ * hinoko_fw_iso_rx_single_unmap_buffer:
+ * @self: A #HinokoFwIsoRxSingle.
+ *
+ * Unmap intermediate buffer shard with 1394 OHCI controller for payload
+ * of IR context.
+ */
+void hinoko_fw_iso_rx_single_unmap_buffer(HinokoFwIsoRxSingle *self)
+{
+	g_return_if_fail(HINOKO_IS_FW_ISO_RX_SINGLE(self));
+
+	hinoko_fw_iso_ctx_unmap_buffer(HINOKO_FW_ISO_CTX(self));
 }

src/fw_iso_rx_single.h
@@ -56,6 +56,12 @@ void hinoko_fw_iso_rx_single_allocate(HinokoFwIsoRxSingle *self,
 				      GError **exception);
 void hinoko_fw_iso_rx_single_release(HinokoFwIsoRxSingle *self);
 
+void hinoko_fw_iso_rx_single_map_buffer(HinokoFwIsoRxSingle *self,
+					guint maximum_bytes_per_payload,
+					guint payloads_per_buffer,
+					GError **exception);
+void hinoko_fw_iso_rx_single_unmap_buffer(HinokoFwIsoRxSingle *self);
+
 G_END_DECLS
 
 #endif

src/hinoko.map
@@ -14,6 +14,8 @@ HINOKO_0_1_0 {
     "hinoko_fw_iso_rx_single_quark";
     "hinoko_fw_iso_rx_single_allocate";
     "hinoko_fw_iso_rx_single_release";
+    "hinoko_fw_iso_rx_single_map_buffer";
+    "hinoko_fw_iso_rx_single_unmap_buffer";
   local:
     *;
 };

tests/fw-iso-rx-single
@@ -15,6 +15,8 @@ methods = (
     'new',
     'allocate',
     'release',
+    'map_buffer',
+    'unmap_buffer',
 )
 vmethods = ()
 signals = ()

commit 4fa8a249244593a6a6db15e8a0e2247715b6464b

fw_iso_rx_single: fulfill start sign of comment block for API documentation
Fixes: ba857a847170 ("fw_iso_rx_single: add APIs to map/unmap packet buffer")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_rx_single.c
@@ -279,7 +279,7 @@ gboolean hinoko_fw_iso_rx_single_allocate(HinokoFwIsoRxSingle *self, const char 
 	return TRUE;
 }
 
-/*
+/**
  * hinoko_fw_iso_rx_single_map_buffer:
  * @self: A [class@FwIsoRxSingle].
  * @maximum_bytes_per_payload: The maximum number of bytes per payload of IR context.
takaswie/libhinoko

commit 6887a02c07dc3dc7e2d06d159871d7b71ad9f220

fw_iso_rx_single: add a method to retrieve packet payload for an abstract method

src/fw_iso_rx_single.c
@@ -21,6 +21,8 @@ struct _HinokoFwIsoRxSinglePrivate {
 	guint chunks_per_irq;
 	guint accumulate_chunk_count;
 	guint maximum_chunk_count;
+
+	const struct fw_cdev_event_iso_interrupt *ev;
 };
 G_DEFINE_TYPE_WITH_PRIVATE(HinokoFwIsoRxSingle, hinoko_fw_iso_rx_single,
 			   HINOKO_TYPE_FW_ISO_CTX)
@@ -63,7 +65,9 @@ static void hinoko_fw_iso_rx_single_class_init(HinokoFwIsoRxSingleClass *klass)
 	 * @count: the number of packets to handle.
 	 *
 	 * When any packet is available, the #HinokoFwIsoRxSingle::interrupted
-	 * signal is emitted with header of the packet.
+	 * signal is emitted with header of the packet. In a handler of the
+	 * signal, payload of received packet is available by a call of
+	 * #hinoko_fw_iso_rx_single_get_payload().
 	 */
 	fw_iso_rx_single_sigs[FW_ISO_RX_SINGLE_SIG_TYPE_INTERRUPTED] =
 		g_signal_new("interrupted",
@@ -280,14 +284,78 @@ void hinoko_fw_iso_rx_single_handle_event(HinokoFwIsoRxSingle *self,
 	count = event->header_length / priv->header_size;
 
 	// TODO; handling error?
+	priv->ev = event;
 	g_signal_emit(self,
 		fw_iso_rx_single_sigs[FW_ISO_RX_SINGLE_SIG_TYPE_INTERRUPTED],
 		0, sec, cycle, event->header, event->header_length, count);
+	priv->ev = NULL;
 
 	for (i = 0; i < priv->chunks_per_irq; ++i) {
 		// Register consumed chunks to reuse.
 		fw_iso_rx_single_register_chunk(self, FALSE, exception);
 		if (*exception != NULL)
 			return;
+	}
+}
+
+/**
+ * hinoko_fw_iso_rx_single_get_payload:
+ * @self: A #HinokoFwIsoRxSingle.
+ * @index: the index inner available packets.
+ * @payload: (element-type guint8) (array length=length) (out callee-allocates):
+ *	     An array with bytes for payload of IR context.
+ * @length: (out): The number of bytes in the above @payload.
+ * @exception: A #GError.
+ *
+ * Retrieve payload of IR context for a handled packet corresponding to index.
+ */
+void hinoko_fw_iso_rx_single_get_payload(HinokoFwIsoRxSingle *self, guint index,
+					 const guint8 **payload, guint *length,
+					 GError **exception)
+{
+	HinokoFwIsoRxSinglePrivate *priv;
+	GValue val = G_VALUE_INIT;
+	unsigned int bytes_per_chunk;
+	unsigned int chunks_per_buffer;
+	unsigned int pos;
+	guint32 iso_header;
+	guint offset;
+	guint frame_size;
+
+	g_return_if_fail(HINOKO_IS_FW_ISO_RX_SINGLE(self));
+	priv = hinoko_fw_iso_rx_single_get_instance_private(self);
+
+	if (priv->ev == NULL) {
+		raise(exception, ENODATA);
+		return;
+	}
+
+	if (index * priv->header_size >= priv->ev->header_length) {
+		raise(exception, EINVAL);
+		return;
+	}
+
+	g_value_init(&val, G_TYPE_UINT);
+	g_object_get_property(G_OBJECT(self), "bytes-per-chunk", &val);
+	bytes_per_chunk = g_value_get_uint(&val);
+	g_value_unset(&val);
+
+	g_value_init(&val, G_TYPE_UINT);
+	g_object_get_property(G_OBJECT(self), "chunks-per-buffer", &val);
+	chunks_per_buffer = g_value_get_uint(&val);
+
+	pos = index * priv->header_size / 4;
+	iso_header = GUINT32_FROM_BE(priv->ev->header[pos]);
+	*length = (iso_header & 0xffff0000) >> 16;
+	if (*length > bytes_per_chunk)
+		*length = bytes_per_chunk;
+
+	index = (priv->accumulate_chunk_count + index) % chunks_per_buffer;
+	offset = index * bytes_per_chunk;
+	hinoko_fw_iso_ctx_read_frames(HINOKO_FW_ISO_CTX(self), offset, *length,
+				      payload, &frame_size);
+	if (frame_size != *length) {
+		raise(exception, EIO);
+		return;
 	}
 }

src/fw_iso_rx_single.h
@@ -57,7 +57,8 @@ struct _HinokoFwIsoRxSingleClass {
 	 *
 	 * When any packet is available, the
 	 * #HinokoFwIsoRxSingleClass::interrupted handler is called with header
-	 * of the packet.
+	 * of the packet. In the handler, payload of received packet is
+	 * available by a call of #hinoko_fw_iso_rx_single_get_payload().
 	 */
 	void (*interrupted)(HinokoFwIsoRxSingle *self, guint sec, guint cycle,
 			    const guint8 *header, guint header_length,
@@ -85,6 +86,10 @@ void hinoko_fw_iso_rx_single_start(HinokoFwIsoRxSingle *self,
 				   HinokoFwIsoCtxMatchFlag tags,
 				   guint packets_per_irq, GError **exception);
 void hinoko_fw_iso_rx_single_stop(HinokoFwIsoRxSingle *self);
+
+void hinoko_fw_iso_rx_single_get_payload(HinokoFwIsoRxSingle *self, guint index,
+					 const guint8 **payload, guint *length,
+					 GError **exception);
 
 G_END_DECLS
 

src/hinoko.map
@@ -19,6 +19,7 @@ HINOKO_0_1_0 {
     "hinoko_fw_iso_rx_single_start";
     "hinoko_fw_iso_rx_single_stop";
     "hinoko_sigs_marshal_VOID__UINT_UINT_POINTER_UINT_UINT";
+    "hinoko_fw_iso_rx_single_get_payload";
   local:
     *;
 };

tests/fw-iso-rx-single
@@ -19,6 +19,7 @@ methods = (
     'unmap_buffer',
     'start',
     'stop',
+    'get_payload',
 )
 vmethods = (
     'do_interrupted',

commit 618aea150ef1889871ed97b92c2d8446a9223eb6

fw_iso_rx_single: fix wrong calculation for length of available payload data
When the length of context header is greater than 8, the part of payload
of isochronous packet is put into context header separated from context
payload. The length of context header is the length of context header
minus 8. In the case, the length of context payload is less than the
length of payload of isochronous packet by the length of context header
minus 8.

Current implementation has wrong calculation for the length of context
payload. When the length of context header is greater than 8, the result
of calculation is always greater than actual length by 8.

This commit fixes the bug.

Fixes: 6887a02c07dc ("fw_iso_rx_single: add a method to retrieve packet payload for an abstract method")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_rx_single.c
@@ -407,6 +407,8 @@ void hinoko_fw_iso_rx_single_get_payload(HinokoFwIsoRxSingle *self, guint index,
 	pos = index * priv->header_size / 4;
 	iso_header = GUINT32_FROM_BE(priv->ev->header[pos]);
 	*length = ieee1394_iso_header_to_data_length(iso_header);
+	if (priv->header_size > 8)
+		*length -= priv->header_size - 8;
 	if (*length > bytes_per_chunk)
 		*length = bytes_per_chunk;
 
70ffdf4f2ed23311d0ce98279c9bf60fc52615fd
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-07-17 18:27:14 +0900

    meson: add dependency to libhinawa v2.6.0 or later
    This commit adds new dependency to libhinawa v2.6.0 or later.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- doc/urlmap.js
+++ doc/urlmap.js
@@ -1,4 +1,5 @@
 baseURLs = [
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Hinawa', 'https://alsa-project.github.io/gobject-introspection-docs/hinawa/'],
 ]

takaswie/libhinoko

commit 8255581c2b9878d706713626a7ca117780f0ed68

fw_iso_ctx: add Hinoko.FwIsoCtx.read_cycle_time() abstract method
This commit adds the new abstract method. The read data is stored to
Hinawa.CycleTime passed by applciation.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_ctx.c
@@ -110,6 +110,31 @@ static void hinoko_fw_iso_ctx_default_init(HinokoFwIsoCtxInterface *iface)
 }
 
 /**
+ * hinoko_fw_iso_ctx_read_cycle_time:
+ * @self: A [iface@FwIsoCtx].
+ * @clock_id: The numeric ID of clock source for the reference timestamp. One CLOCK_REALTIME(0),
+ *	      CLOCK_MONOTONIC(1), and CLOCK_MONOTONIC_RAW(4) is available in UAPI of Linux kernel.
+ * @cycle_time: (inout): A [struct@Hinawa.CycleTime] to store data of cycle time.
+ * @error: A [struct@GLib.Error].
+ *
+ * Retrieve the value of cycle time register. This method call is available once any isochronous
+ * context is created.
+ *
+ * Returns: TRUE if the overall operation finishes successfully, otherwise FALSE.
+ *
+ * Since: 0.9.
+ */
+gboolean hinoko_fw_iso_ctx_read_cycle_time(HinokoFwIsoCtx *self, gint clock_id,
+					   HinawaCycleTime *const *cycle_time, GError **error)
+{
+	g_return_val_if_fail(HINOKO_IS_FW_ISO_CTX(self), FALSE);
+	g_return_val_if_fail(cycle_time != NULL, FALSE);
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+	return HINOKO_FW_ISO_CTX_GET_IFACE(self)->read_cycle_time(self, clock_id, cycle_time, error);
+}
+
+/**
  * hinoko_fw_iso_ctx_get_cycle_timer:
  * @self: A [iface@FwIsoCtx].
  * @clock_id: The numeric ID of clock source for the reference timestamp. One CLOCK_REALTIME(0),

src/fw_iso_ctx.h
@@ -49,6 +49,24 @@ struct _HinokoFwIsoCtxInterface {
 	void (*release)(HinokoFwIsoCtx *self);
 
 	/**
+	 * HinokoFwIsoCtxInterface::read_cycle_time:
+	 * @self: A [iface@FwIsoCtx].
+	 * @clock_id: The numeric ID of clock source for the reference timestamp. One CLOCK_REALTIME(0),
+	 *	      CLOCK_MONOTONIC(1), and CLOCK_MONOTONIC_RAW(4) is available in UAPI of Linux kernel.
+	 * @cycle_time: (inout): A [struct@Hinawa.CycleTime] to store data of cycle time.
+	 * @error: A [struct@GLib.Error].
+	 *
+	 * Virtual function to tetrieve the value of cycle time register. This method call is
+	 * available once any isochronous context is created.
+	 *
+	 * Returns: TRUE if the overall operation finishes successfully, otherwise FALSE.
+	 *
+	 * Since: 0.9.
+	 */
+	gboolean (*read_cycle_time)(HinokoFwIsoCtx *self, gint clock_id,
+				    HinawaCycleTime *const *cycle_time, GError **error);
+
+	/**
 	 * HinokoFwIsoCtxInterface::get_cycle_timer:
 	 * @self: A [iface@FwIsoCtx].
 	 * @clock_id: The numeric ID of clock source for the reference timestamp. One CLOCK_REALTIME(0),
@@ -112,6 +130,9 @@ void hinoko_fw_iso_ctx_stop(HinokoFwIsoCtx *self);
 void hinoko_fw_iso_ctx_unmap_buffer(HinokoFwIsoCtx *self);
 
 void hinoko_fw_iso_ctx_release(HinokoFwIsoCtx *self);
+
+gboolean hinoko_fw_iso_ctx_read_cycle_time(HinokoFwIsoCtx *self, gint clock_id,
+					   HinawaCycleTime *const *cycle_time, GError **error);
 
 gboolean hinoko_fw_iso_ctx_get_cycle_timer(HinokoFwIsoCtx *self, gint clock_id,
 					   HinokoCycleTimer *const *cycle_timer, GError **error);

src/fw_iso_ctx_private.c
@@ -606,6 +606,39 @@ gboolean fw_iso_ctx_state_flush_completions(struct fw_iso_ctx_state *state, GErr
 }
 
 /**
+ * fw_iso_ctx_state_read_cycle_time:
+ * @state: A [struct@FwIsoCtxState].
+ * @clock_id: The numeric ID of clock source for the reference timestamp. One CLOCK_REALTIME(0),
+ *	      CLOCK_MONOTONIC(1), and CLOCK_MONOTONIC_RAW(4) is available in UAPI of Linux kernel.
+ * @cycle_time: (inout): A [struct@Hinawa.CycleTime] to store data of cycle time.
+ * @error: A [struct@GLib.Error].
+ *
+ * Retrieve the value of cycle time register. This method call is available once any isochronous
+ * context is created.
+ *
+ * Returns: TRUE if the overall operation finishes successfully, otherwise FALSE.
+ */
+gboolean fw_iso_ctx_state_read_cycle_time(struct fw_iso_ctx_state *state, gint clock_id,
+					  HinawaCycleTime *const *cycle_time, GError **error)
+{
+	g_return_val_if_fail(cycle_time != NULL && *cycle_time != NULL, FALSE);
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+	if (state->fd < 0) {
+		generate_fw_iso_ctx_error_coded(error, HINOKO_FW_ISO_CTX_ERROR_NOT_ALLOCATED);
+		return FALSE;
+	}
+
+	(*cycle_time)->clk_id = clock_id;
+	if (ioctl(state->fd, FW_CDEV_IOC_GET_CYCLE_TIMER2, *cycle_time) < 0) {
+		generate_fw_iso_ctx_error_ioctl(error, errno, FW_CDEV_IOC_GET_CYCLE_TIMER2);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+/**
  * fw_iso_ctx_state_get_cycle_timer:
  * @state: A [struct@FwIsoCtxState].
  * @clock_id: The numeric ID of clock source for the reference timestamp. One CLOCK_REALTIME(0),

src/fw_iso_ctx_private.h
@@ -123,6 +123,9 @@ void fw_iso_ctx_state_read_frame(struct fw_iso_ctx_state *state, guint offset, g
 
 gboolean fw_iso_ctx_state_flush_completions(struct fw_iso_ctx_state *state, GError **error);
 
+gboolean fw_iso_ctx_state_read_cycle_time(struct fw_iso_ctx_state *state, gint clock_id,
+					  HinawaCycleTime *const *cycle_time, GError **error);
+
 gboolean fw_iso_ctx_state_get_cycle_timer(struct fw_iso_ctx_state *state, gint clock_id,
 					  HinokoCycleTimer *const *cycle_timer, GError **error);
 

src/hinoko.map
@@ -84,3 +84,7 @@ HINOKO_0_8_0 {
     "hinoko_fw_iso_it_register_packet";
     "hinoko_fw_iso_it_start";
 } HINOKO_0_7_0;
+
+HINOKO_0_9_0 {
+    "hinoko_fw_iso_ctx_read_cycle_time";
+} HINOKO_0_8_0;

src/hinoko.h
@@ -8,6 +8,8 @@
 #include <linux/firewire-cdev.h>
 #include <linux/firewire-constants.h>
 
+#include <libhinawa/hinawa.h>
+
 #include <hinoko_sigs_marshal.h>
 
 #include <hinoko_enum_types.h>

tests/fw-iso-ctx
@@ -18,6 +18,7 @@ methods = (
     'stop',
     'unmap_buffer',
     'release',
+    'read_cycle_time',
     'get_cycle_timer',
     'create_source',
     'flush_completions',
@@ -26,6 +27,7 @@ vmethods = (
     'do_stop',
     'do_unmap_buffer',
     'do_release',
+    'do_read_cycle_time',
     'do_get_cycle_timer',
     'do_flush_completions',
     'do_create_source',

commit e5133b71942403e67a39b22f6455c89f1d2c620e

fw_iso_ctx: change prototype of FwIsoCtx.read_cycle_time()
The prototype includes redundant type expression for Hinawa.CycleTime. This
commit simplifies it.

Fixes: 8255581c2b98 ("fw_iso_ctx: add Hinoko.FwIsoCtx.read_cycle_time() abstract method")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_ctx.c
@@ -121,10 +121,10 @@ static void hinoko_fw_iso_ctx_default_init(HinokoFwIsoCtxInterface *iface)
  *
  * Returns: TRUE if the overall operation finishes successfully, otherwise FALSE.
  *
- * Since: 0.9
+ * Since: 1.0.
  */
 gboolean hinoko_fw_iso_ctx_read_cycle_time(HinokoFwIsoCtx *self, gint clock_id,
-					   HinawaCycleTime *const *cycle_time, GError **error)
+					   HinawaCycleTime **cycle_time, GError **error)
 {
 	g_return_val_if_fail(HINOKO_IS_FW_ISO_CTX(self), FALSE);
 	g_return_val_if_fail(cycle_time != NULL, FALSE);

src/fw_iso_ctx.h
@@ -64,7 +64,7 @@ struct _HinokoFwIsoCtxInterface {
 	 * Since: 0.9
 	 */
 	gboolean (*read_cycle_time)(HinokoFwIsoCtx *self, gint clock_id,
-				    HinawaCycleTime *const *cycle_time, GError **error);
+				    HinawaCycleTime **cycle_time, GError **error);
 
 	/**
 	 * HinokoFwIsoCtxInterface::flush_completions:
@@ -114,7 +114,7 @@ void hinoko_fw_iso_ctx_unmap_buffer(HinokoFwIsoCtx *self);
 void hinoko_fw_iso_ctx_release(HinokoFwIsoCtx *self);
 
 gboolean hinoko_fw_iso_ctx_read_cycle_time(HinokoFwIsoCtx *self, gint clock_id,
-					   HinawaCycleTime *const *cycle_time, GError **error);
+					   HinawaCycleTime **cycle_time, GError **error);
 
 gboolean hinoko_fw_iso_ctx_create_source(HinokoFwIsoCtx *self, GSource **source, GError **error);
 

src/fw_iso_ctx_private.c
@@ -619,7 +619,7 @@ gboolean fw_iso_ctx_state_flush_completions(struct fw_iso_ctx_state *state, GErr
  * Returns: TRUE if the overall operation finishes successfully, otherwise FALSE.
  */
 gboolean fw_iso_ctx_state_read_cycle_time(struct fw_iso_ctx_state *state, gint clock_id,
-					  HinawaCycleTime *const *cycle_time, GError **error)
+					  HinawaCycleTime **cycle_time, GError **error)
 {
 	g_return_val_if_fail(cycle_time != NULL && *cycle_time != NULL, FALSE);
 	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);

src/fw_iso_ctx_private.h
@@ -124,7 +124,7 @@ void fw_iso_ctx_state_read_frame(struct fw_iso_ctx_state *state, guint offset, g
 gboolean fw_iso_ctx_state_flush_completions(struct fw_iso_ctx_state *state, GError **error);
 
 gboolean fw_iso_ctx_state_read_cycle_time(struct fw_iso_ctx_state *state, gint clock_id,
-					  HinawaCycleTime *const *cycle_time, GError **error);
+					  HinawaCycleTime **cycle_time, GError **error);
 
 gboolean fw_iso_ctx_state_create_source(struct fw_iso_ctx_state *state, HinokoFwIsoCtx *inst,
 					gboolean (*handle_event)(HinokoFwIsoCtx *inst,

src/fw_iso_ir_multiple.c
@@ -173,7 +173,7 @@ static void fw_iso_ir_multiple_release(HinokoFwIsoCtx *inst)
 }
 
 static gboolean fw_iso_ir_multiple_read_cycle_time(HinokoFwIsoCtx *inst, gint clock_id,
-						   HinawaCycleTime *const *cycle_time,
+						   HinawaCycleTime **cycle_time,
 						   GError **error)
 {
 	HinokoFwIsoIrMultiple *self;

src/fw_iso_ir_single.c
@@ -140,7 +140,7 @@ static void fw_iso_ir_single_release(HinokoFwIsoCtx *inst)
 }
 
 static gboolean fw_iso_ir_single_read_cycle_time(HinokoFwIsoCtx *inst, gint clock_id,
-						 HinawaCycleTime *const *cycle_time,
+						 HinawaCycleTime **cycle_time,
 						 GError **error)
 {
 	HinokoFwIsoIrSingle *self;

src/fw_iso_it.c
@@ -131,7 +131,7 @@ static void fw_iso_it_release(HinokoFwIsoCtx *inst)
 }
 
 static gboolean fw_iso_it_read_cycle_time(HinokoFwIsoCtx *inst, gint clock_id,
-					  HinawaCycleTime *const *cycle_time, GError **error)
+					  HinawaCycleTime **cycle_time, GError **error)
 {
 	HinokoFwIsoIt *self;
 	HinokoFwIsoItPrivate *priv;

src/hinoko.map
@@ -73,7 +73,6 @@ HINOKO_0_8_0 {
 } HINOKO_0_7_0;
 
 HINOKO_0_9_0 {
-    "hinoko_fw_iso_ctx_read_cycle_time";
 } HINOKO_0_8_0;
 
 HINOKO_1_0_0 {
@@ -85,4 +84,6 @@ HINOKO_1_0_0 {
 
     "hinoko_fw_iso_resource_auto_deallocate";
     "hinoko_fw_iso_resource_auto_deallocate_wait";
+
+    "hinoko_fw_iso_ctx_read_cycle_time";
 } HINOKO_0_9_0;
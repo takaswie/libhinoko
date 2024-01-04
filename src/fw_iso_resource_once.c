takaswie/libhinoko

commit edfa5ad1f45d07375a4bee162e349d243e81d83a

fw_iso_resource_once: add derived object to initiate allocation/deallocation by one shot
Current implementataion of Hinoko.FwIsoResource object includes functions
one-shot allocation/deallocation as well as basic functions for derived
objects. It's inconvenient in the derived object side.

This commit adds Hinoko.FwIsoResourceOnce object for future use to move
the one-shot functions.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_resource_once.c
@@ -1,0 +1,37 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "internal.h"
+
+/**
+ * HinokoFwIsoResourceOnce:
+ * An object to initiate requests and listen events of isochronous resource allocation/deallocation
+ * by one shot.
+ *
+ * The [class@FwIsoResourceOnce] is an object to initiate requests and listen events of isochronous
+ * resource allocation/deallocation by file descriptor owned internally. The allocated resource
+ * is left even if this object is destroyed, thus application is responsible for deallocation.
+ */
+G_DEFINE_TYPE(HinokoFwIsoResourceOnce, hinoko_fw_iso_resource_once, HINOKO_TYPE_FW_ISO_RESOURCE)
+
+static void hinoko_fw_iso_resource_once_class_init(HinokoFwIsoResourceOnceClass *klass)
+{
+	return;
+}
+
+static void hinoko_fw_iso_resource_once_init(HinokoFwIsoResourceOnce *self)
+{
+	return;
+}
+
+/**
+ * hinoko_fw_iso_resource_once_new:
+ *
+ * Allocate and return an instance of [class@FwIsoResourceOnce].
+ *
+ * Returns: A [class@FwIsoResourceOnce].
+ *
+ * Sine: 0.7.
+ */
+HinokoFwIsoResourceOnce *hinoko_fw_iso_resource_once_new()
+{
+	return g_object_new(HINOKO_TYPE_FW_ISO_RESOURCE_ONCE, NULL);
+}

src/fw_iso_resource_once.h
@@ -1,0 +1,22 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __HINOKO_FW_ISO_RESOURCE_ONCE_H__
+#define __HINOKO_FW_ISO_RESOURCE_ONCE_H__
+
+#include <hinoko.h>
+
+G_BEGIN_DECLS
+
+#define HINOKO_TYPE_FW_ISO_RESOURCE_ONCE	(hinoko_fw_iso_resource_once_get_type())
+
+G_DECLARE_DERIVABLE_TYPE(HinokoFwIsoResourceOnce, hinoko_fw_iso_resource_once, HINOKO,
+			 FW_ISO_RESOURCE_ONCE, HinokoFwIsoResource);
+
+struct _HinokoFwIsoResourceOnceClass {
+	HinokoFwIsoResourceClass parent_class;
+};
+
+HinokoFwIsoResourceOnce *hinoko_fw_iso_resource_once_new();
+
+G_END_DECLS
+
+#endif

src/hinoko.h
@@ -22,5 +22,6 @@
 
 #include <fw_iso_resource.h>
 #include <fw_iso_resource_auto.h>
+#include <fw_iso_resource_once.h>
 
 #endif

src/hinoko.map
@@ -93,4 +93,7 @@ HINOKO_0_7_0 {
   global:
     "hinoko_fw_iso_resource_auto_allocate_sync";
     "hinoko_fw_iso_resource_auto_deallocate_sync";
+
+    "hinoko_fw_iso_resource_once_get_type";
+    "hinoko_fw_iso_resource_once_new";
 } HINOKO_0_5_0;

src/meson.build
@@ -14,6 +14,7 @@ sources = [
   'cycle_timer.c',
   'fw_iso_resource.c',
   'fw_iso_resource_auto.c',
+  'fw_iso_resource_once.c',
 ]
 
 headers = [
@@ -25,6 +26,7 @@ headers = [
   'cycle_timer.h',
   'fw_iso_resource.h',
   'fw_iso_resource_auto.h',
+  'fw_iso_resource_once.h',
   'hinoko_enum_types.h'
 ]
 

tests/fw-iso-resource-once
@@ -1,0 +1,22 @@
+#!/usr/bin/env python3
+
+from sys import exit
+from errno import ENXIO
+
+from helper import test
+
+import gi
+gi.require_version('Hinoko', '0.0')
+from gi.repository import Hinoko
+
+target = Hinoko.FwIsoResourceOnce()
+props = ()
+methods = (
+    'new',
+)
+vmethods = ()
+signals = ()
+
+if not test(target, props, methods, vmethods, signals):
+    exit(ENXIO)
+

tests/meson.build
@@ -6,6 +6,7 @@ tests = [
   'fw-iso-tx',
   'fw-iso-resource',
   'fw-iso-resource-auto',
+  'fw-iso-resource-once',
 ]
 
 envs = environment()

commit bf953ee7325950985da70470772bad9f1253aaa3

fw_iso_resource_once: fix support version annotation
Fixes: edfa5ad1f45d ("fw_iso_resource_once: add derived object to initiate allocation/deallocation by one shot")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_iso_resource_once.c
@@ -195,7 +195,7 @@ static void fw_iso_resource_iface_init(HinokoFwIsoResourceInterface *iface)
  *
  * Returns: A [class@FwIsoResourceOnce].
  *
- * Sine: 0.7.
+ * Since: 0.7
  */
 HinokoFwIsoResourceOnce *hinoko_fw_iso_resource_once_new()
 {